45839893712be23170ffee104a70f8c9b71892b7
dlyubich <lyubichdi@gmail.com>
2018-07-17 12:28:53 +0300

    Malloc protection

--- simpledb.c
+++ simpledb.c
@@ -115,12 +115,13 @@ enum err listdir(const char *dirname, struct strvec *vector)
 		goto cleanup_item_listdir;
 	}
 
-	vector->vec = malloc(n * sizeof *vector->vec);
+	if (NULL == (vector->vec = malloc(n * sizeof *vector->vec)))
+		return (E_OSERR);
 	/* Copy contents to vec. If error occurs, stop copy and stick with what we have */
 	int i;
 	for (i = 0; i < n; i++) {
 		vector->vec[i] = strdup(namelist[i]->d_name);
-		if (vector->vec == NULL) break;
+		if (vector->vec[i] == NULL) break;
 	}
 	vector->n = i;
 	if (i != n)
